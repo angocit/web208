5c9a1a032b6e86429232f8e599543ce23a297f71
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-05-08 16:09:53 +0700

    B2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl:'./app.component.html',
+  styles: `
+    h1{text-transform:uppercase}
+  `
 })
 export class AppComponent {
   title = 'web208';
