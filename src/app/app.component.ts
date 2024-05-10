4892163438db527fa955a8414770eb04e829cf31
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-05-10 15:16:23 +0700

    B3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from './interface/product';
+import { IStudent } from './interface/student';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web208';
+  students:IStudent[]=[
+    {
+      name: "SV FPL 1",
+      age: 18,
+      address:"Hà Nội",
+      classname:'WD18333'
+    },
+    {
+      name: "SV FPL 2",
+      age: 20,
+      address:"Trịnh Văn Bô",
+      classname:'WD18333'
+    }
+  ]
 }
