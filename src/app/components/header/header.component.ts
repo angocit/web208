ea736ff31543882e540a3034232169ec7c3dfb25
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-05-15 15:26:08 +0700

    B5

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMenu } from '../../interface/menu';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  menus:IMenu[]=[
+    {
+      id:1,
+      name:'Home',
+      path:'home',
+      parent:0
+    },
+    {
+    id:2,
+    name:'About us',
+    path:'about-us',
+    parent:0
+    },
+    {
+    id:3,
+    name:'Products',
+    path:'product',
+    parent:0
+    },
+    {
+    id:4,
+    name:'Fashion',
+    path:'fashion',
+    parent:3
+    },
+    {
+    id:5,
+    name:'Jewelry',
+    path:'jewelry',
+    parent:3
+    },
+    {
+    id:6,
+    name:'Contact',
+    path:'contact',
+    parent:0
+    }
+  ]
+  checkChildrent = (id:number,menu:IMenu[]):boolean=>{
+    let check:boolean = false;
+      menu.forEach(item=>{
+        if (item.parent==id){
+          check = true;
+        }
+      })
+    return check;
+  }
 }
