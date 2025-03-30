0c08d4795349ebba07216e64825decd8b596f45a
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-03-30 10:00:43 +0700

    b9:Phân quyền route

---
+++ src/app/Guard/admin.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivateFn, Router } from '@angular/router';
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  // let user:any = localStorage.getItem("user")
+  // if (user){
+  //   user = JSON.parse(user)
+  // }
+  const router = new Router()
+  const user = {
+    username:'admin',
+    role:'user'
+  }
+  if (user.role =='admin')
+  {
+    return true
+  }
+  else {
+    router.navigate(['login'])
+    return false
+  }
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductaddComponent } from './components/product/productadd/productadd.component';
 import { ProducteditComponent } from './components/product/productedit/productedit.component';
+import { LoginComponent } from './components/login/login.component';
+import { adminGuard } from './Guard/admin.guard';
 
 export const routes: Routes = [
-    {path:"product-add",component:ProductaddComponent},
+    {path:"product-add",component:ProductaddComponent,canActivate:[adminGuard]},
     {path:"product-edit/:id",component:ProducteditComponent},
+    {path:"login",component:LoginComponent},
 ];
