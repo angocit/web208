eae384fbb8db325ef73df263f44fce9f5ac56140
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-03-31 10:27:40 +0700

    b10: fill dữ liệu vào form

--- src/app/components/product/productedit/productedit.component.ts
+++ src/app/components/product/productedit/productedit.component.ts
@@ -1,18 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-productedit',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './productedit.component.html',
   styleUrl: './productedit.component.css'
 })
 export class ProducteditComponent {
+  productform = new FormGroup({
+      name: new FormControl("",[Validators.required,Validators.minLength(6)]),
+      image: new FormControl(""),
+      price: new FormControl(0)
+    })
    routes = inject(ActivatedRoute)
-   ngOnInit(){
-    const id = this.routes.snapshot.params["id"]
-    const kw = this.routes.snapshot.queryParams["keyword"]
-    console.log(`ID là: ${id}`);   
-    console.log(`Keyword là: ${kw}`); 
+   id = this.routes.snapshot.params["id"]
+   async ngOnInit(){    
+    await this.getProductById(this.id)
+   }
+   getProductById = async (id:number)=>{
+      try {
+        const {data} = await axios.get(`http://localhost:3000/products/${id}`)
+        this.productform.controls.name.setValue(data.name)
+        this.productform.controls.image.setValue(data.image)
+        this.productform.controls.price.setValue(data.price)
+        // console.log(data);
+        
+      } catch (error) {
+        
+      }
+   }
+   async onSubmit(){
+      if (this.productform.valid){
+          try {
+              await axios.put(`http://localhost:3000/products/${this.id}`,this.productform.value)
+              alert("cập nhật thành công")
+          } catch (error:any) {
+            alert(error.response.data)
+          }
+      }
    }
 }
