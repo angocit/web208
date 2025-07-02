fea2e0ccc0800e6d029777af8e366a79f47cf263
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-07-02 15:15:23 +0700

    b2:Tích hợp taiwind và chèn component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./component/header/header";
+import { Slide } from "./component/slide/slide";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Slide],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
