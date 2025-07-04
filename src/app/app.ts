0347e00acd90e1c3966ac8dd0eafe347229742a3
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-07-04 15:15:33 +0700

    b3:thực hành call API

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./component/header/header";
 import { Slide } from "./component/slide/slide";
-
+import { IProduct } from './interface/product';
+import axios from 'axios'
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, Slide],
@@ -11,4 +12,18 @@ import { Slide } from "./component/slide/slide";
 })
 export class App {
   protected title = 'wd20105';
+  products:IProduct[] = []
+  ngOnInit(){
+    this.title = "Giá trị mới"
+    this.getAllProducts()
+  }
+  getAllProducts = async ()=>{
+    try {
+        const {data} = await axios.get(`http://localhost:3000/products`)
+        console.log(data);        
+        this.products=data
+    } catch (error) {
+      
+    }
+  }
 }
---
+++ src/app/interface/product.ts
@@ -0,0 +1,5 @@
+export interface IProduct {
+  ten_san_pham: string;
+  hinh_anh: string;
+  gia_tien: number;
+}
