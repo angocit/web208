0f1399a5268fa0a33b8ef60be0e4899f5a3674fe
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-07-04 14:28:19 +0700

    b3: thực hành với @for, @if

--- src/app/component/header/header.ts
+++ src/app/component/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMenu } from '../../interface/menu';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
-
+   title:string = "wd20105"
+   member:string[]=["Ngoc","Minh","Linh","An"]
+   menus:IMenu[]=[
+    {
+      id:1,
+      name:"Trang chủ",
+      url:"/"
+    },
+    {
+      id:2,
+      name:"Sản phẩm",
+      url:"/shop"
+    },
+    {
+      id:3,
+      name:"Tin tức",
+      url:"/tin-tuc"
+    },
+    {
+      id:4,
+      name:"Liên hệ",
+      url:"/lien-he"
+    }
+   ]
 }
---
+++ src/app/interface/menu.ts
@@ -0,0 +1,5 @@
+export interface IMenu {
+    id:number,
+    name:string,
+    url:string
+}
