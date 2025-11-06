bc82fb542382eff442fcdcc4f2742da32ed08116
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-11-06 17:29:51 +0700

    b2: @for,@if

--- src/app/layouts/header/header.ts
+++ src/app/layouts/header/header.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+interface IMenu{
+  id:number,
+  name:string,
+  url:string
+}
 @Component({
   selector: 'app-header',
   imports: [],
@@ -9,6 +13,24 @@ import { Component } from '@angular/core';
 export class Header {
     logo:string = "WD20204"
     display(){
-      return "ABCDFSDFSD"
+      return `Xin chào ${this.logo}`
     }
+    menus:IMenu[] = [
+      {
+        id:1,
+        name:"Trang chủ",
+        url:"/"
+      },
+      {
+        id:2,
+        name:"Shop",
+        url:"/shop"
+      },
+      {
+        id:3,
+        name:"Liên hệ",
+        url:"/contact"
+      }
+    ]
+    show:boolean = true 
 }
