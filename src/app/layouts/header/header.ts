d12c5e3522340a0b6688b6a18cdd527a516b1a7c
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-11-06 17:37:04 +0700

    b2: Menu mẫu

--- src/app/layouts/header/header.ts
+++ src/app/layouts/header/header.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 interface IMenu{
   id:number,
   name:string,
-  url:string
+  url:string,
+  parent:number
 }
 @Component({
   selector: 'app-header',
@@ -19,17 +20,38 @@ export class Header {
       {
         id:1,
         name:"Trang chủ",
-        url:"/"
+        url:"/",
+        parent:0
       },
       {
         id:2,
         name:"Shop",
-        url:"/shop"
+        url:"/shop",
+        parent:0
       },
       {
         id:3,
         name:"Liên hệ",
-        url:"/contact"
+        url:"/contact",
+        parent:0
+      },
+      {
+        id:4,
+        name:"Quần áo",
+        url:"/quan-ao",
+        parent:2
+      },
+      {
+        id:5,
+        name:"Giày dép",
+        url:"/giay-dep",
+        parent:2
+      },
+      {
+        id:6,
+        name:"Phụ kiện",
+        url:"/phu-kien",
+        parent:2
       }
     ]
     show:boolean = true 
