2674d66039e6e4cca7112563eca482c2f3bf062e
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-11-06 16:48:05 +0700

    b1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layouts/header/header";
+import { Footer } from "./layouts/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styles:`
+    h1{
+      color:blue;
+      font-size: 64px
+    }
+  `
 })
 export class App {
-  protected readonly title = signal('wd20204');
+  // protected readonly title = signal('wd20204');
+  title = "WD20204"
 }
