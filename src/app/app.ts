7d17f5fbe4be5ad6a64c800950ca743e51d1fc24
unknown <angocit@gmail.com>
2025-11-10 19:54:54 +0700

    b4: Chữa bài lab 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layouts/header/header";
 import { Footer } from "./layouts/footer/footer";
-
+interface IProduct {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+}
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, Footer],
@@ -12,8 +17,21 @@ import { Footer } from "./layouts/footer/footer";
 export class App {
   // protected readonly title = signal('wd20203');
   show:boolean = false
+  products:IProduct[] = []
   handleClick=()=>{
     // alert("Bạn vừa click")
     this.show = !this.show
+  }
+  LoadProduct = async ()=>{
+    try {
+        const res = await fetch('http://localhost:3000/products')
+        const data = await res.json()
+        this.products = data
+    } catch (error) {
+        console.log(error);        
+    }
+  }
+  handleDelete = (id:number)=>{
+    this.products = this.products.filter(product=>product.id!=id)
   }
 }
