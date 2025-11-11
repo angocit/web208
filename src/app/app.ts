5c87ce7796bb07706f52c9ed7b29e3a5a2e48760
unknown <angocit@gmail.com>
2025-11-11 17:38:15 +0700

    b4: Sử dụng @Input()

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layouts/header/header";
 import { Footer } from "./layouts/footer/footer";
+import { FormsModule } from '@angular/forms';
+import { IProduct } from '../interface/product';
+import { Productitem } from './conponents/productitem/productitem';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer,FormsModule,Productitem],
   templateUrl: './app.html',
   styles:`
     h1{
@@ -18,7 +21,15 @@ export class App {
   // protected readonly title = signal('wd20204');
   title = "WD20204"
   show:boolean = false
+  products:IProduct[]=[
+		{ id: 1, name: "Áo thun basic", image: "images/ao-thun.jpg", price: 199000 },
+		{ id: 2, name: "Quần jeans xanh", image: "images/quan-jeans.jpg", price: 349000 },
+		{ id: 3, name: "Giày sneakers", image: "images/giay-sneakers.jpg", price: 599000 },
+		{ id: 4, name: "Mũ lưỡi trai", image: "images/mu-luoi-trai.jpg", price: 129000 },
+		{ id: 5, name: "Áo khoác denim", image: "images/ao-khoac-denim.jpg", price: 799000}
+	]
   handleClick=()=>{
     this.show= !this.show
   }
+
 }
---
+++ src/app/conponents/productitem/productitem.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { IProduct } from '../../../interface/product';
+
+@Component({
+  selector: 'app-productitem',
+  imports: [],
+  templateUrl: './productitem.html',
+  styleUrl: './productitem.css',
+})
+export class Productitem {
+  @Input() product:IProduct = {} as IProduct
+}
