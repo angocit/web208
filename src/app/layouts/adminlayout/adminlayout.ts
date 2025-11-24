7892b62cf89b4d46aa56d5cb271dbfc57bf1d9f9
unknown <angocit@gmail.com>
2025-11-24 19:01:38 +0700

    B10: template admin

--- src/app/layouts/adminlayout/adminlayout.ts
+++ src/app/layouts/adminlayout/adminlayout.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Header } from './header/header';
+import { Sidebar } from './sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-adminlayout',
-  imports: [],
+  imports: [Header,Sidebar,RouterOutlet],
   templateUrl: './adminlayout.html',
   styleUrl: './adminlayout.css',
 })
 export class Adminlayout {
-
+bt_status:boolean = false;
+  onClickButton=(value:boolean)=>{
+    // console.log(value);  
+    this.bt_status = value  
+  }
 }
