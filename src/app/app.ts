ecc06f7b9d3bef7faecfd9a216314f8ae6073018
angocit <angocit@gmail.com>
2026-03-11 20:02:06 +0700

    b2:data bindding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { NgClass } from "../../node_modules/@angular/common/types/_common_module-chunk";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet, Header,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   // protected readonly title = signal('wd20309');
+  title:string = 'Xin chào WD20309'
+  name='text-red'
+  show:boolean = false
+  background:string = 'yellow'
+  keywords:string = 'Từ khóa'
+  handleClick = ()=>{
+    this.name = 'ahihihi'
+    // this.show = true
+    // if (this.show){
+    //   this.show = false
+    // }
+    // else {
+    //   this.show=true
+    // }
+    this.show = !this.show
+    this.background = 'blue'
+  }
 }
