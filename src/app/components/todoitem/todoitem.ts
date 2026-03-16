d14c62a13c80791b935e717b42c3c7b7a48b3f7c
angocit <angocit@gmail.com>
2026-03-16 19:51:58 +0700

    b4:Sử dụng @Input cách viết khác

--- src/app/components/todoitem/todoitem.ts
+++ src/app/components/todoitem/todoitem.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { ITodo } from '../../interfaces/todo';
-
+type TTodo={
+  todo:ITodo,
+  isFirst:boolean,
+  isLast:boolean,
+  index:number
+}
 @Component({
   selector: 'app-todoitem',
   imports: [],
   templateUrl: './todoitem.html',
   styleUrl: './todoitem.css',
 })
+
 export class Todoitem {
-  @Input() todo:ITodo = {} as ITodo
-  @Input() isFirst:boolean = true
-  @Input() isLast:boolean = true
-  @Input() index:number = 0
+  @Input() todoinfo:TTodo = {} as TTodo
 }
