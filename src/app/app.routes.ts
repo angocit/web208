6054c8f99a39fd122892fd1848f38d35997148b1
angocit <angocit@gmail.com>
2026-03-20 20:28:11 +0700

    b6: Chia router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Client } from './layout/client/client';
+import { Admin } from './layout/admin/admin';
+import { Home } from './pages/home/home';
+import { Detail } from './pages/detail/detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Client,children:[
+        {path:'',component:Home},
+        {path:'detail',component:Detail}
+    ]},
+    {path:'admin',component:Admin}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -9,67 +9,11 @@ import type { IPost } from './interfaces/post';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header,FormsModule,Todoitem],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   // protected readonly title = signal('wd20309');
-  title:string = 'Xin chào WD20309'
-  changedt = inject(ChangeDetectorRef)
-  // name:string='text-red'
-  show:boolean = false
-  background:string = 'yellow'
-  keywords:string = 'Từ khóa'
-  name:string = ''
-  message:string = ''
-  priority:string = ''
-  posts:IPost[] = []
-  todos:ITodo[]= [
-    {
-      name:"Đi học",
-      priority:"Cao"
-    },
-    {
-      name:"Đi làm thêm",
-      priority:"Trung bình"
-    }
-  ]
-  handleClick = ()=>{
-    this.name = 'ahihihi'
-    // this.show = true
-    // if (this.show){
-    //   this.show = false
-    // }
-    // else {
-    //   this.show=true
-    // }
-    // this.show = !this.show
-    this.background = 'blue'
-  }
-  handleShowHide = ()=>{
-      this.show = !this.show
-  }
-  addTodos =()=>{
-     const newtodo:ITodo = {
-      name:this.name,
-      priority:this.priority
-     }
-    //  console.log(newtodo);
-     this.todos.push(newtodo)
-     this.name=''
-     this.priority=''
-  }
-  CurrentName =(text:string)=>{
-    this.message = text
-  }
-  async ngOnInit(){
-     try {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`)
-        this.posts = await res.json()
-        this.changedt.markForCheck()
-     } catch (error) {
-      
-     }
-  }
+  
 }
