481eca5b8e707ecaa6a2f1ad88f6cbcf62fe27fb
angocit <angocit@gmail.com>
2026-03-23 19:29:55 +0700

    b7: sử dụng router-link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { Client } from './layout/client/client';
 import { Admin } from './layout/admin/admin';
 import { Home } from './pages/home/home';
 import { Detail } from './pages/detail/detail';
+import { Productlist } from './pages/admin/productlist/productlist';
+import { Productadd } from './pages/admin/productadd/productadd';
+import { Productedit } from './pages/admin/productedit/productedit';
 
 export const routes: Routes = [
     {path:'',component:Client,children:[
         {path:'',component:Home},
         {path:'detail',component:Detail}
     ]},
-    {path:'admin',component:Admin}
+    {path:'admin',component:Admin, children:[
+        {path:'products',component:Productlist},
+        {path:'products/add',component:Productadd},
+        {path:'products/edit/:id',component:Productedit}
+    ]}
 ];
--- src/app/layout/admin/admin.ts
+++ src/app/layout/admin/admin.ts
@@ -3,9 +3,10 @@ import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { RouterLink } from "@angular/router";
 @Component({
   selector: 'app-admin',
-  imports: [NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule],
+  imports: [NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule, RouterLink],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
