775d2c6675049eb2ceb04ffb8f1a2545a458e9d0
angocit <angocit@gmail.com>
2026-03-24 14:35:24 +0700

    b7: Tích hợp antdesign

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideNzIcons } from 'ng-zorro-antd/icon';
 import { routes } from './app.routes';
-
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { AccountBookFill, AlertFill, MenuFoldOutline, TeamOutline, UserOutline } from '@ant-design/icons-angular/icons';
+registerLocaleData(en);
+const icons: IconDefinition[] = [UserOutline,TeamOutline,MenuFoldOutline];
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideBrowserGlobalErrorListeners(), 
+    provideRouter(routes), 
+    provideNzI18n(en_US),
+    provideNzIcons(icons)
+  ],
 };
--- src/app/layout/adminlayout/adminlayout.ts
+++ src/app/layout/adminlayout/adminlayout.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 @Component({
   selector: 'app-adminlayout',
-  imports: [],
+  imports: [NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule],
   templateUrl: './adminlayout.html',
   styleUrl: './adminlayout.css',
 })
-export class Adminlayout {}
+export class Adminlayout {
+  isCollapsed = false;
+  protected readonly date = new Date();
+}
