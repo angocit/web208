562b9ddcef40b1fad4ee1b1efd766725cf61e836
angocit <angocit@gmail.com>
2026-03-31 15:21:05 +0700

    b10: sử dụng form

--- src/app/pages/admin/productadd/productadd.ts
+++ src/app/pages/admin/productadd/productadd.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
-
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-productadd',
-  imports: [],
+  imports: [ReactiveFormsModule,NzFormModule,NzInputModule,NzButtonModule],
   templateUrl: './productadd.html',
   styleUrl: './productadd.css',
 })
-export class Productadd {}
+export class Productadd {
+  productform = new FormGroup({
+    name: new FormControl('',Validators.required),
+    image:new FormControl(''),
+    price: new FormControl(0,[Validators.required,Validators.min(1000)])
+  })
+  onSubmit = ()=>{
+      // Lấy data từ form
+      const productdata = this.productform.value
+      console.log(productdata);
+      
+  }
+}
